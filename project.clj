65ad3ad38a86cde01ad6fce194459b11a3df1fa7
Donevan Dolby <dolbydc@gmail.com>
2021-05-16 16:18:27 +0000

    Removed core.async which I did not end up using.

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject record-parser "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.async "0.2.374" :exclusions [org.clojure/tools.reader]]
                  [org.clojure/tools.cli "1.0.206"]
                  [ring "1.9.0"]
                  [ring/ring-jetty-adapter "1.9.3"]
